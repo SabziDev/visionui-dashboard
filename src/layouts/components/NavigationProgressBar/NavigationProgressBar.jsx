7526594acc755a056a66a09bb18ff55856547b38
Abolfazl Sabzmohammadi <SabziDev@gmail.com>
2026-07-20 09:10:08 +0330

    add: shadow to NavigationProgressBar

--- src/layouts/components/NavigationProgressBar/NavigationProgressBar.jsx
+++ src/layouts/components/NavigationProgressBar/NavigationProgressBar.jsx
@@ -1,8 +1,11 @@
+import clsx from "clsx";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router";
 
-const PROGRESS_BAR_COLOR = "bg-blue";
+const COLOR = "#0075ff";
+
 const SHOW_DURATION = 0.8;
 const COMPLETE_DURATION = 0.2;
 const HIDE_DELAY = 500;
@@ -11,14 +14,16 @@ const PROGRESS_KEYFRAMES = [
 ];
 
 const NavigationProgressBar = () => {
-  const { pathname } = useLocation();
-
-  const isPageFirstLoadRef = useRef(true);
-  const completeTimeoutRef = useRef();
-  const hideTimeoutRef = useRef();
+  const { i18n } = useTranslation();
+  const DIR = i18n.dir();
 
   const [isShow, setIsShow] = useState(false);
   const [isCompleted, setIsCompleted] = useState(false);
+  const isPageFirstLoadRef = useRef(true);
+  const completeTimeoutRef = useRef(null);
+  const hideTimeoutRef = useRef(null);
+
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (isPageFirstLoadRef.current) {
@@ -29,14 +34,12 @@ const NavigationProgressBar = () => {
 
     clearTimeout(completeTimeoutRef.current);
     clearTimeout(hideTimeoutRef.current);
-
     setIsShow(true);
     setIsCompleted(false);
 
     completeTimeoutRef.current = setTimeout(() => {
       setIsCompleted(true);
     }, SHOW_DURATION * 1000);
-
     hideTimeoutRef.current = setTimeout(
       () => {
         setIsShow(false);
@@ -64,7 +67,18 @@ const NavigationProgressBar = () => {
             duration: isCompleted ? COMPLETE_DURATION : SHOW_DURATION,
             ease: "easeOut",
           }}
-          className={`fixed top-0 left-0 z-9999 h-1 w-full origin-left ${PROGRESS_BAR_COLOR}`}
+          className={clsx([
+            "fixed z-9999 h-1 w-full",
+            "after:absolute after:top-0 after:h-full after:w-12 after:content-['']",
+            "after:shadow-[0_0_20px_4px_var(--progress-bar-color)]",
+            DIR === "ltr"
+              ? "origin-left after:right-0"
+              : "origin-right after:left-0",
+          ])}
+          style={{
+            "--progress-bar-color": COLOR,
+            backgroundColor: "var(--progress-bar-color)",
+          }}
         />
       )}
     </AnimatePresence>
