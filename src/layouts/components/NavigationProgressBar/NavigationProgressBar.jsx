71a8c1a4bc9d7fa5842f8e292abf8e8ba5ef1b67
Abolfazl Sabzmohammadi <SabziDev@gmail.com>
2026-08-23 10:04:41 +0330

    update: NavigationProgressBar

--- src/layouts/components/NavigationProgressBar/NavigationProgressBar.jsx
+++ src/layouts/components/NavigationProgressBar/NavigationProgressBar.jsx
@@ -2,107 +2,75 @@ import clsx from "clsx";
 import { AnimatePresence, motion } from "motion/react";
 import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigation } from "react-router";
+import { useLocation } from "react-router";
 
 const COLOR = "#0075ff";
 
-const MIN_SHOW_TIME = 300;
-const COMPLETE_TIME = 200;
+const SHOW_DURATION = 0.8;
+const COMPLETE_DURATION = 0.2;
 const HIDE_DELAY = 500;
-const PROGRESS_POINTS = [0.05, 0.12, 0.25, 0.4, 0.55, 0.68, 0.75, 0.82, 0.95];
+const PROGRESS_KEYFRAMES = [
+  0.02, 0.08, 0.2, 0.38, 0.55, 0.72, 0.84, 0.92, 0.96,
+];
 
 const NavigationProgressBar = () => {
-  const [isShow, setIsShow] = useState(false);
-  const [progress, setProgress] = useState(0);
-
-  const startTimeRef = useRef(0);
-  const progressIndexRef = useRef(0);
-  const progressTimeoutRef = useRef(null);
-  const hideTimeoutRef = useRef(null);
-
-  const { state } = useNavigation();
   const { i18n } = useTranslation();
   const DIR = i18n.dir();
 
-  const isLoading = state === "loading";
+  const [isShow, setIsShow] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(false);
+  const isPageFirstLoadRef = useRef(true);
+  const completeTimeoutRef = useRef(null);
+  const hideTimeoutRef = useRef(null);
+
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    if (isLoading) {
-      startTimeRef.current = Date.now();
-
-      clearTimeout(progressTimeoutRef.current);
-      clearTimeout(hideTimeoutRef.current);
-
-      setIsShow(true);
-      setProgress(0);
-
-      progressIndexRef.current = 0;
-
-      const increaseProgress = () => {
-        const next = PROGRESS_POINTS[progressIndexRef.current] ?? 0.95;
-
-        setProgress(next);
-
-        progressIndexRef.current += 1;
-
-        if (progressIndexRef.current < PROGRESS_POINTS.length) {
-          progressTimeoutRef.current = setTimeout(increaseProgress, 300);
-        }
-      };
-
-      increaseProgress();
+    if (isPageFirstLoadRef.current) {
+      isPageFirstLoadRef.current = false;
 
       return;
     }
 
-    if (isShow) {
-      clearTimeout(progressTimeoutRef.current);
+    clearTimeout(completeTimeoutRef.current);
+    clearTimeout(hideTimeoutRef.current);
+    setIsShow(true);
+    setIsCompleted(false);
 
-      const elapsedTime = Date.now() - startTimeRef.current;
-      const remainingTime = MIN_SHOW_TIME - elapsedTime;
-
-      const complete = () => {
-        setProgress(1);
-
-        hideTimeoutRef.current = setTimeout(() => {
-          setIsShow(false);
-          setProgress(0);
-        }, COMPLETE_TIME + HIDE_DELAY);
-      };
-
-      if (remainingTime > 0) {
-        hideTimeoutRef.current = setTimeout(complete, remainingTime);
-      } else {
-        complete();
-      }
-    }
+    completeTimeoutRef.current = setTimeout(() => {
+      setIsCompleted(true);
+    }, SHOW_DURATION * 1000);
+    hideTimeoutRef.current = setTimeout(
+      () => {
+        setIsShow(false);
+      },
+      SHOW_DURATION * 1000 + COMPLETE_DURATION * 1000 + HIDE_DELAY,
+    );
 
     return () => {
-      clearTimeout(progressTimeoutRef.current);
+      clearTimeout(completeTimeoutRef.current);
       clearTimeout(hideTimeoutRef.current);
     };
-  }, [isLoading, isShow]);
+  }, [pathname]);
 
   return (
     <AnimatePresence>
       {isShow && (
         <motion.div
+          key={pathname}
           initial={{ scaleX: 0 }}
           animate={{
-            scaleX: progress,
+            scaleX: isCompleted ? 1 : PROGRESS_KEYFRAMES,
           }}
-          exit={{
-            opacity: 0,
-          }}
+          exit={{ opacity: 0 }}
           transition={{
-            duration: progress === 1 ? COMPLETE_TIME / 1000 : 0.3,
+            duration: isCompleted ? COMPLETE_DURATION : SHOW_DURATION,
             ease: "easeOut",
           }}
           className={clsx([
             "fixed z-9999 h-1 w-full",
             "after:absolute after:top-0 after:h-full after:w-12 after:content-['']",
             "after:shadow-[0_0_20px_4px_var(--progress-bar-color)]",
-
             DIR === "ltr"
               ? "origin-left after:right-0"
               : "origin-right after:left-0",
