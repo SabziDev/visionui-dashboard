0ee4035bdc60eaf21b4432e0d40dff1370f84a02
Abolfazl Sabzmohammadi <SabziDev@gmail.com>
2026-09-04 11:56:23 +0330

    update: folder-structure

--- src/pages/auth/Signin/components/BgImg/BgImg.jsx
+++ src/pages/auth/Signin/components/BgImg/BgImg.jsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 const BgImg = ({ className = "" }) => {
   return (
     <img
-      src="/images/pictures/pages/public/signin/sandro-katalina.webp"
+      src="/images/pictures/pages/auth/signin/sandro-katalina.webp"
       alt="bg-brand"
       loading="lazy"
       className={clsx(["absolute inset-0 size-full object-cover", className])}
