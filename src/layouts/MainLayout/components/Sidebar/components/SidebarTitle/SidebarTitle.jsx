461eff4eb19bc75f926c5899cb3d628f392de651
Abolfazl Sabzmohammadi <SabziDev@gmail.com>
2026-09-10 22:55:00 +0330

    remove viewTransition-prop from SidebarTitle

--- src/layouts/MainLayout/components/Sidebar/components/SidebarTitle/SidebarTitle.jsx
+++ src/layouts/MainLayout/components/Sidebar/components/SidebarTitle/SidebarTitle.jsx
@@ -6,7 +6,7 @@ const SidebarTitle = ({ onCloseSidebar }) => {
   const { t } = useTranslation();
 
   return (
-    <Link to="/" viewTransition onClick={onCloseSidebar} className="w-full">
+    <Link to="/" onClick={onCloseSidebar} className="w-full">
       <h1
         className={clsx([
           "mb-5 from-[#E0E1E2]/90 via-[#E0E1E2]/70 to-[#E0E1E2]/0 bg-clip-text text-center font-VazirBold tracking-ultra-wide text-transparent",
