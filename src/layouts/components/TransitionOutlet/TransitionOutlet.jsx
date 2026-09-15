f010a5da184b10ea51443350d7ad049eb1e69c56
Abolfazl Sabzmohammadi <SabziDev@gmail.com>
2026-09-15 11:57:02 +0330

    update: vscode tasks

--- src/layouts/components/TransitionOutlet/TransitionOutlet.jsx
+++ src/layouts/components/TransitionOutlet/TransitionOutlet.jsx
@@ -1,23 +1,11 @@
-/* eslint-disable custom/add-blank-line-before-jump-statement */
-
-import { useEffect, useRef, useState, ViewTransition } from "react";
-import { useLocation } from "react-router";
+import { ViewTransition } from "react";
 
 const TransitionOutlet = ({ children }) => {
-  const { pathname } = useLocation();
-
-  const previousPathnameRef = useRef(pathname);
-  const [isRouteChanged, setIsRouteChanged] = useState(false);
-
-  useEffect(() => {
-    const hasChanged = previousPathnameRef.current !== pathname;
-
-    setIsRouteChanged(hasChanged);
-    previousPathnameRef.current = pathname;
-  }, [pathname]);
-
-  if (!isRouteChanged) return children;
-  return <ViewTransition name="page-content">{children}</ViewTransition>;
+  return (
+    <ViewTransition update="page" default="none">
+      {children}
+    </ViewTransition>
+  );
 };
 
 export default TransitionOutlet;
